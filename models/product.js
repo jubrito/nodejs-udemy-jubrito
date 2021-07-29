8a022eab5a7f01f2a2a53028e4117e957e256fa6
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-07-29 14:31:14 -0300

    Refactors the File Storage Code, adds getProductsFromFile and changes callbackWhenFetchAllIsDone

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
 
 // the data is the product itself, not the products
-// ARRAY-DATA: saving data in an array 
+// ARRAY-DATA: if you choose to save data in an array 
 // const products = []; // 1. Array-data
 const fs = require('fs');
 const path = require('path');
@@ -11,6 +11,15 @@ const filePath = path.join(
     'data', 
     'products.json'
 );
+const getProductsFromFile = (callbackWhenFetchAllIsDone) => {
+    fs.readFile(filePath, async (error, fileContentJSON) => {
+        if (error) {
+            return callbackWhenFetchAllIsDone([]);
+        }
+        const fileArrayToBePassedThroughCallback = JSON.parse(fileContentJSON);
+        callbackWhenFetchAllIsDone(fileArrayToBePassedThroughCallback); // parse returns an array
+    })
+}
 module.exports = class Product {
     constructor(title) {
         this.title = title;
@@ -18,26 +27,16 @@ module.exports = class Product {
 
     //  this = object created based on the class
     save() {
-        // products.push(this); // 2. Array-data
-        fs.readFile(filePath, (error, fileContentJSON) => {
-            let products = [];
-            if (!error) {
-                products = JSON.parse(fileContentJSON); // from json to array
-            }
+        getProductsFromFile(products => { // passing the callback function while writing the code that actually saves 
             products.push(this);
             fs.writeFile(filePath, JSON.stringify(products), (error) => { // from array to json
                 console.log(error);
-            }) 
+            })
         });
+        // products.push(this); // 2. Array-data
     }
 
     static fetchAll(callbackWhenFetchAllIsDone) {
-        fs.readFile(filePath, async (error, fileContentJSON) => {
-            if (error) {
-                callbackWhenFetchAllIsDone([])
-            }
-            const fileArrayToBePassedThroughCallback = JSON.parse(fileContentJSON);
-            callbackWhenFetchAllIsDone(fileArrayToBePassedThroughCallback); // parse returns an array
-        })
+       getProductsFromFile(callbackWhenFetchAllIsDone);
     }
 }
