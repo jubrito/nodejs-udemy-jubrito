2d4102b7609462160578d45b6072e68d9241b19e
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-08-04 11:17:53 -0300

    Separates the no sequelize structure and connects to the database using sequelize

--- util/database.js
+++ util/database.js
@@ -1,15 +1,33 @@
 const mysql = require('mysql2');
 
+
 const hostOrIpAddresss = 'localhost';
 const schemaCreatedOnMySql = 'nodejs_udemy';
+const rootUsername = 'root';
+const rootPassword = 'juju2009';
+/* WITHOUT SEQUELIZE 
 const connectionPool = mysql.createPool({
     host: hostOrIpAddresss,
-    user: 'root',
+    user: rootUsername
     database: schemaCreatedOnMySql,
-    password: 'juju2009'
+    password: rootPassword
 });
 
-/* Promice chains: use promises when working with these connections which 
-handle asynchronous tasks, asynchronous data instead of many nested callbacks 
-because promises allow us to write code in a bit more structured way*/
+// Promice chains: use promises when working with these connections which 
+// handle asynchronous tasks, asynchronous data instead of many nested callbacks 
+// because promises allow us to write code in a bit more structured way
 module.exports = connectionPool.promise(); 
+*/
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+    schemaCreatedOnMySql,
+    rootUsername,
+    rootPassword,
+    {
+        dialect: 'mysql',
+        host: hostOrIpAddresss
+    }
+)
+
+module.exports = sequelize;
