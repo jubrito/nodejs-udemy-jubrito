ccec1e3a6c62554aaf8601a069c708d5d579e2df
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-08-04 15:55:38 -0300

    Adds One-To-Many relationship between user and products

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ const adminRoutes = require('./routes/admin');
 const productRoutes = require('./routes/product');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database'); 
 
+const sequelize = require('./util/database'); 
+const Product = require('./models/product'); 
+const User = require('./models/user'); 
 
 //  PARSE REQUEST BODY MIDDLEWARE 
 app.use(express.urlencoded({ extended: false }));
@@ -23,9 +25,16 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+const howTheRelashionshipWillBeManaged = {
+    constraints: true,
+    onDelete: 'CASCADE' // if you delete an user any products related will also be deleted
+}
+Product.belongsTo(User, howTheRelashionshipWillBeManaged);
+User.hasMany(Product);
+
 // Syncs the model you created to the database by creating the table and if you have them, relations
 sequelize
-    .sync()
+    .sync({ force: true }) // force true only if you want to overwright tables (CAUTION PRODUCTION)
     .then(result => {
         app.listen(8080) 
     })
