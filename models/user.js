d870265a1d3b4689cdf4cb81fa2175c1ebcba1d0
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-08-23 11:08:21 -0300

    Works on cart items & orders adding a cart to the user model when adding a product

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, _id) {
     this.username = username;
     this.email = email;
+    this.cart = cart;
+    this._id = this._id;
   }
 
   save() {
@@ -14,6 +16,18 @@ class User {
     return db
       .collection('users')
       .insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProducts = this.cart.items.fndIndex(cartProduct => {
+      return cartProduct._id = product._id
+    });
+    const updatedCart = {items: [{...product, quantity: 1}]};
+    const db = getDb();
+    db.collection('users').updateOne(
+      {_id: new ObjectId(this._id)}, 
+      {$set: {cart: updatedCart}}
+    );
   }
 
   static findById(userId) {
