482f6fa27cce49f31957120c71451caf83043613
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-09-09 17:42:08 -0300

    Configures cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,16 @@
 exports.getLogin = (req, res, next) => {
     const cookieNameAndValue = req.get('Cookie');
-    const cookieValue = cookieNameAndValue.trim().split('=')[1];
+    const isAuthenticated = cookieNameAndValue.trim().split('=')[1];
     console.log(cookieNameAndValue);
     console.log(cookieValue);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'login',
-        isAuthenticated: req.isAuthenticated, 
+        isAuthenticated: isAuthenticated, 
     })
 }
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'isAuthenticated=true');
+    res.setHeader('Set-Cookie', 'isAuthenticated=true;');
     res.redirect('/');
 }
