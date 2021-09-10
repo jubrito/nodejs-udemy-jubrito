8fce7f5dd462ef79a951f83e0ae7b6283d0f843c
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-09-10 15:09:55 -0300

    Data stored across requests but not across users using sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,17 @@
 exports.getLogin = (req, res, next) => {
-    const cookieNameAndValue = req.get('Cookie');
-    const isAuthenticated = cookieNameAndValue.trim().split('=')[1];
-    console.log(cookieNameAndValue);
-    console.log(cookieValue);
+    // const cookieNameAndValue = req.get('Cookie');
+    // const isAuthenticated = cookieNameAndValue.trim().split('=')[1];
+    // console.log(cookieNameAndValue);
+    // console.log(cookieValue);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'login',
-        isAuthenticated: isAuthenticated, 
+        isAuthenticated: req.session.isAuthenticated, 
     })
 }
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'isAuthenticated=true; httpOnly');
+    // res.setHeader('Set-Cookie', 'isAuthenticated=true; httpOnly');
+    req.session.isAuthenticated = true;
     res.redirect('/');
 }
