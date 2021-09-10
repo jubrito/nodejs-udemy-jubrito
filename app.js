f23ee5418f289ea4845873814cb9dd5861797606
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-09-10 15:30:45 -0300

    Uses MongoDB to store sessions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const expressSession = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(expressSession);
 
 const adminRoutes = require('./routes/admin');
 const productRoutes = require('./routes/product');
@@ -12,6 +13,15 @@ const User = require('./models/user');
 
 
 const app = express();
+const USERNAME_MONGODB = 'juliana';
+const PASSWORD_MONGODB = 'ar6tE3vMlcpFT4OW';
+const DATABASE_I_WANT_TO_CONNECT = 'shop';
+const CONNECTION_STRING_FROM_MONGODB_WEBSITE_CLUSTER = `mongodb+srv://${USERNAME_MONGODB}:${PASSWORD_MONGODB}@clusterbackend0.luzfp.mongodb.net/${DATABASE_I_WANT_TO_CONNECT}`;
+const store = new MongoDBStore({
+    uri: CONNECTION_STRING_FROM_MONGODB_WEBSITE_CLUSTER,
+    collection: 'sessions',
+    // expires
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -25,6 +35,7 @@ app.use(expressSession({
     secret: 'this text will be used for signing the hash which secretly stores our ID in the cookie.',
     resave: false, // session will only be saved when the session changes, it won't be saved on every request done on every response sent
     saveUninitialized: false, // ensure that no session gets saved for a request where it doesn't need to be saved because nothing was cahnged about it
+    store: store,
     // cookie: {maxAge}
 }))
 
@@ -45,12 +56,9 @@ app.use(shopRoutes);
 app.use(authRoutes); 
 app.use(errorController.get404);
 
-const username = 'juliana';
-const password = 'ar6tE3vMlcpFT4OW';
-const databaseIWantToConnect = 'shop';
-const connectionStringFromMongodbWebsiteCluster = `mongodb+srv://${username}:${password}@clusterbackend0.luzfp.mongodb.net/${databaseIWantToConnect}?retryWrites=true&w=majority`;
+
 mongoose
-    .connect(connectionStringFromMongodbWebsiteCluster)
+    .connect(CONNECTION_STRING_FROM_MONGODB_WEBSITE_CLUSTER)
     .then(connectionResult => {
         User.findOne().then(user => {
             if (!user) {
