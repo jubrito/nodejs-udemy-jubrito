75e04f8d9574b6bb3907371d12c5c4e87eb7d8bf
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-09-15 13:16:00 -0300

    Adds the sign in funtionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,18 +14,33 @@ exports.getLogin = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
     User
-    .findById('612cf29d744acef2c2f9d419')
-    .then(user => {
-        req.session.isAuthenticated = true;
-        req.session.user = user;
-        req.session.save((err) => {
-             // make sure the session was created before you continue, so it doesn't redirect before updating the session
-             console.log(err);
-             res.redirect('/'); // redirect only after creating the session
-        });
-    })
-    .catch(err => console.log(err))
+        .findOne({ email: email})
+        .then(user => {
+            if (!user) {
+                return res.redirect('/login');
+            }
+            bcryptjs
+                .compare(password, user.password)
+                .then(hashedPasswordIsEqualToFielPassword => {
+                    if (hashedPasswordIsEqualToFielPassword) {
+                        req.session.isAuthenticated = true;
+                        req.session.user = user;
+                        return req.session.save((err) => {
+                            // make sure the session was created before you continue, so it doesn't redirect before updating the session
+                            console.log(err);
+                            res.redirect('/'); // with save we redirect only after creating the session
+                        });
+                    }
+                    return res.redirect('/login');
+                })
+                .catch(err => {
+                    res.redirect('/');
+                    console.log(err);
+                }) 
+        }).catch(err => console.log(err));
 }
 
 exports.postLogout = (req, res, next) => {
