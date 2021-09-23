77864d718495ba949210c86cfa1d22c377b8136b
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-09-23 11:48:39 -0300

    Re-uses pagination logic and controls

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,32 @@
 const Product = require('../models/product');
 
-exports.getProducts = (req, resp, next) => {
-    Product.find()
+const ITEMS_PER_PAGE = 1;
+
+exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalNumberOfProducts;
+    Product
+        .find()
+        .countDocuments()
+        .then(numberOfProducts => {
+            totalNumberOfProducts = numberOfProducts;
+            return Product
+                .find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        })
         .then(products => {
-            resp.render('shop/product-list', { 
-                products: products, 
-                pageTitle: 'All products', 
-                path: '/products', 
-            }); 
+            res.render('shop/product-list', {
+                products: products,
+                pageTitle: 'All products',
+                path: '/products',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalNumberOfProducts,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalNumberOfProducts / ITEMS_PER_PAGE) // ceil rounds up
+        });
         }).catch(error => {
             console.log(error)
         });
