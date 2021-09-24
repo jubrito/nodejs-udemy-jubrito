d198650d113ae1b3edac5417cfdcbc8ac5dc3dd9
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-09-24 14:23:32 -0300

    Creates our REST API Project & Implements the Route Setup

---
+++ routes/feed.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const feedController = require('../controllers/feed');
+const router = express.Router();
+
+router.get('/posts', feedController.getPosts);
+
+module.exports = router;
