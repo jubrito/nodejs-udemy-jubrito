b2e4a03a1ff2f878913b0082c5f559f344d88ed2
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-10-04 18:08:24 -0300

    Changes all 'then/catch' to 'async/await' on controllers

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -26,11 +26,9 @@ exports.getPosts = async (req, res, next) => {
     const perPage = 2;
     try {
         const numberOfItens = await Post.find().countDocuments();
-        const posts = await Post.find()
+        const posts = await Post.find().populate('creator')
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
-        console.log('posts')
-        console.log(posts)
         res.status(STATUS_SUCCESS).json({ 
             message: 'Fetched posts successfully', 
             posts: posts, 
@@ -42,7 +40,7 @@ exports.getPosts = async (req, res, next) => {
 }
 
 
-exports.postPosts = (req, res, next) => {
+exports.postPosts = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Validation failed, entered data is not valid', STATUS_VALIDATION_FAILED_ERROR);
@@ -53,54 +51,43 @@ exports.postPosts = (req, res, next) => {
     const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
-    let creator;
     const post = new Post({
         title: title, 
         content: content,
         imageUrl: imageUrl,
         creator: req.userId,
     })
-    post
-        .save()
-        .then(result => {
-            return User.findById(req.userId);
-        })
-        .then(user => {
-            creator = user;
-            user.posts.push(post);
-            return user.save();
-        })
-        .then(userUpdated => {
-            res
-                .status(STATUS_SUCCESS_RESOURCE_WAS_CREATED)
-                .json({
-                    message: 'Post created successfully',
-                    post: post,
-                    creator: { _id: creator._id, name: creator.name }
-                });
-        })
-        .catch(err => {
-            handleErrorsOnAsyncCodeUsingNext(err, next, STATUS_SERVER_SIDE_ERROR);
-        }); 
-    
+    try {
+        const postCreated = await post.save();
+        const user = await User.findById(req.userId);
+        user.posts.push(post);
+        const userUpdated = await user.save();
+        res
+            .status(STATUS_SUCCESS_RESOURCE_WAS_CREATED)
+            .json({
+                message: 'Post created successfully',
+                post: post,
+                creator: { _id: user._id, name: user.name }
+            });
+    } catch(err) {
+        handleErrorsOnAsyncCodeUsingNext(err, next, STATUS_SERVER_SIDE_ERROR);
+    }
 }
 
-exports.getPost = (req, res, next) => {
+exports.getPost = async (req, res, next) => {
     const postId = req.params.postId;
-    Post
-        .findById(postId)
-        .then(post => {
-            if (!post) {
-                throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock(next, STATUS_SERVER_SIDE_ERROR);
-            }
-            res.status(STATUS_SUCCESS).json({ message: 'Post fetched', post: post})
-        })
-        .catch(err => {
-            handleErrorsOnAsyncCodeUsingNext(err, next);
-        })
+    try {
+        const post = await Post.findById(postId);
+        if (!post) {
+            throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock(next, STATUS_SERVER_SIDE_ERROR);
+        }
+        res.status(STATUS_SUCCESS).json({ message: 'Post fetched', post: post})
+    } catch(err) {
+        handleErrorsOnAsyncCodeUsingNext(err, next);
+    }
 }
 
-exports.updatePost = (req, res, next) => {
+exports.updatePost = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Validation failed, entered data is not valid', STATUS_VALIDATION_FAILED_ERROR);
@@ -115,63 +102,49 @@ exports.updatePost = (req, res, next) => {
     if (!imageUrl) {
         throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('No file uploaded', STATUS_VALIDATION_FAILED_ERROR);
     }
-    Post
-        .findById(postId)
-        .then(post => {
-            const userIsTryingToUpdateAPostCreatedBySomeoneElse = post.creator.toString() !== req.userId;
-            if (!post) {
-                throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Could not find post', STATUS_VALIDATION_FAILED_ERROR);
-            }
-            if (userIsTryingToUpdateAPostCreatedBySomeoneElse) {
-                throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Not authorized', STATUS_FORBIDDEN);
-            }
-            if (imageUrl !== post.imageUrl) {
-                clearImage(post.imageUrl);
-            }
-            post.title = title;
-            post.content - content;
-            post.imageUrl = imageUrl;
-            return post.save();
-        })
-        .then(result => {
-            console.log('result')
-            console.log(result)
-            return res.status(STATUS_SUCCESS).json({ message: 'Post updated', post: result })
-        })
-        .catch(err => {
-            handleErrorsOnAsyncCodeUsingNext(err, next);
-        })
+    try {
+        const post = await Post.findById(postId);
+        const userIsTryingToUpdateAPostCreatedBySomeoneElse = post.creator.toString() !== req.userId;
+        if (!post) {
+            throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Could not find post', STATUS_VALIDATION_FAILED_ERROR);
+        }
+        if (userIsTryingToUpdateAPostCreatedBySomeoneElse) {
+            throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Not authorized', STATUS_FORBIDDEN);
+        }
+        if (imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+        post.title = title;
+        post.content - content;
+        post.imageUrl = imageUrl;
+        const newPost = await post.save();
+        return res.status(STATUS_SUCCESS).json({ message: 'Post updated', post: newPost })
+    } catch(err) {
+        handleErrorsOnAsyncCodeUsingNext(err, next);
+    }
 }
 
-exports.deletePost = (req, res, next) => {
+exports.deletePost = async (req, res, next) => {
     const postId = req.params.postId;
-    Post
-        .findById(postId)
-        .then(post => {
-            if (!post) {
-                throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Could not find post', STATUS_VALIDATION_FAILED_ERROR);
-            }
-            const userIsTryingToUpdateAPostCreatedBySomeoneElse = post.creator.toString() !== req.userId;
-            if (userIsTryingToUpdateAPostCreatedBySomeoneElse) {
-                throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Not authorized', STATUS_FORBIDDEN);
-            }
-            clearImage(post.imageUrl);
-            return Post.findById(postId).remove();
-        })
-        .then(postWasRemoved => {
-            return User.findById(req.userId);
-        })
-        .then(userMongooseModelFetchedFromDatabase => {
-            // clear the relation between user and posts removing the post from the user model once it is deleted
-            userMongooseModelFetchedFromDatabase.posts.pull(postId);
-            return userMongooseModelFetchedFromDatabase.save();
-        })
-        .then(result => {
-            res.status(STATUS_SUCCESS).json({ message: 'Post was deleted' })
-        })
-        .catch(err => {
-            handleErrorsOnAsyncCodeUsingNext(err, next);
-        });
+    try {
+        const post = await Post.findById(postId);
+        if (!post) {
+            throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Could not find post', STATUS_VALIDATION_FAILED_ERROR);
+        }
+        const userIsTryingToUpdateAPostCreatedBySomeoneElse = post.creator.toString() !== req.userId;
+        if (userIsTryingToUpdateAPostCreatedBySomeoneElse) {
+            throwErrorsOnSyncOrAsyncPassingToTheClosestCatchBlock('Not authorized', STATUS_FORBIDDEN);
+        }
+        clearImage(post.imageUrl);
+        await Post.findByIdAndDelete(postId);
+        const userMongooseModelFetchedFromDatabase = await User.findById(req.userId);
+        // clear the relation between user and posts removing the post from the user model once it is deleted
+        userMongooseModelFetchedFromDatabase.posts.pull(postId);
+        await userMongooseModelFetchedFromDatabase.save();
+        res.status(STATUS_SUCCESS).json({ message: 'Post was deleted' })
+    } catch(err) {
+        handleErrorsOnAsyncCodeUsingNext(err, next);
+    };
 }
 
 const clearImage = filePath => {
