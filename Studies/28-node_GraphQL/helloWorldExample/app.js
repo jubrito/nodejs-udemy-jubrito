5ca95a614dc8dfb1957f2f045ee44bc60457101c
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2021-10-08 14:10:00 -0300

    Adds graphql simple example to studies folder

---
+++ Studies/28-node_GraphQL/helloWorldExample/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const { graphqlHTTP } = require('express-graphql');
+const graphqlSchema = require('./helloWorldExample/graphql/schema');
+const graphqlResolvers = require('./helloWorldExample/graphql/resolvers');
+const USERNAME_MONGODB = 'juliana';
+const PASSWORD_MONGODB = 'ar6tE3vMlcpFT4OW';
+const DATABASE_I_WANT_TO_CONNECT = 'messages';
+const CONNECTION_STRING_FROM_MONGODB_WEBSITE_CLUSTER = `mongodb+srv://${USERNAME_MONGODB}:${PASSWORD_MONGODB}@clusterbackend0.luzfp.mongodb.net/${DATABASE_I_WANT_TO_CONNECT}`;
+
+app.use('/graphql', graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers
+}));
+
+/* Test in postmand  
+GET http://localhost:8080/graphql
+Body -> Raw (JSON)
+{
+    "query": "{ hello { text } }"
+}
+*/
+
+mongoose
+    .connect(CONNECTION_STRING_FROM_MONGODB_WEBSITE_CLUSTER)
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => { console.log(err)});
