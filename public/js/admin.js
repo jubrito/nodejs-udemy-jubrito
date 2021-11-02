436c0c306cd23831fe1b8a52233759d282a13ce1
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2021-11-02 18:13:47 -0300

    Changes delete js function to avoid 'inline event handler' error

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,6 @@
-const deleteProduct = (deleteButton) => {
+const deleteButton = document.getElementById("delete-product")
+
+const deleteProduct = () => {
     const productId = deleteButton.parentNode.querySelector('[name=productId]').value;
     const csrfToken = deleteButton.parentNode.querySelector('[name=_csrf]').value;
     const productElement = deleteButton.closest('article');
@@ -17,3 +19,5 @@ const deleteProduct = (deleteButton) => {
     })
     .catch(err => {console.log(err)})
 }
+
+deleteButton.addEventListener("click", deleteProduct);
