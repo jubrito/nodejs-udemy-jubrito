87c62c234a65f48609cf9e1e5dc8b71bea40ecf2
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2021-11-22 15:02:30 -0300

    Tests Auth Middleware using .bind()

--- Studies/27-node_REST_socketio/server/test/auth-middleware.js
+++ Studies/27-node_REST_socketio/server/test/auth-middleware.js
@@ -1,9 +1,15 @@
+const expect = require('chai').expect;
 const authMiddleware = require('../middlewares/is-auth');
 
 it('should throw an error when no authorization header is present', function () {
-    const req = {
+    const reqWithoutHeader = {
         get: function() {
-            
+            return null;
         }
-    }
+    };
+    const response = {};
+    
+    // Bind allows to pass a function reference to Mocha and Chai instead of running the function (and throwing an error).
+    expect(authMiddleware.bind(this, reqWithoutHeader, response, () => {})).to.throw("Not Authenticated");
+
 })
