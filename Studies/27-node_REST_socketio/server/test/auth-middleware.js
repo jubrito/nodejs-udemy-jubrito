16ef36fca123ce00aff13344ab656b9e9970621a
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2021-11-22 15:07:31 -0300

    Organizes multiple tests

--- Studies/27-node_REST_socketio/server/test/auth-middleware.js
+++ Studies/27-node_REST_socketio/server/test/auth-middleware.js
@@ -1,15 +1,24 @@
 const expect = require('chai').expect;
 const authMiddleware = require('../middlewares/is-auth');
 
-it('should throw an error when no authorization header is present', function () {
-    const reqWithoutHeader = {
-        get: function() {
-            return null;
-        }
-    };
-    const response = {};
-    
-    // Bind allows to pass a function reference to Mocha and Chai instead of running the function (and throwing an error).
-    expect(authMiddleware.bind(this, reqWithoutHeader, response, () => {})).to.throw("Not Authenticated");
-
+describe('Auth middleware', function() {
+    const emptyResponse = {};
+    const emptyNextFunction = () => {};
+    it('should throw an error when no authorization header is present', function () {
+        const reqWithoutHeader = {
+            get: function() {
+                return null;
+            }
+        };
+        // Bind allows to pass a function reference to Mocha and Chai instead of running the function (and throwing an error).
+        expect(authMiddleware.bind(this, reqWithoutHeader, emptyResponse, emptyNextFunction)).to.throw("Not Authenticated");
+    });
+    it('should throw and error if the authorization header is only one string', function () {
+        const req = {
+            get: function() {
+                return 'incompleteauthorizationheader';
+            }
+        };
+        expect(authMiddleware.bind(this, req, emptyResponse, emptyNextFunction)).to.throw();
+    });
 })
