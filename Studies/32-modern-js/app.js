c8c577723057fd7453975231e9501da173ab266c
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2022-04-13 15:52:09 -0300

    Importing with and without curly brackets

--- Studies/32-modern-js/app.js
+++ Studies/32-modern-js/app.js
@@ -1,9 +1,14 @@
-const express = require('express');
+// const express = require('express');
+// const responseHandler = require('./response-handler');
+import express from 'express';
 
-const responseHandler = require('./response-handler');
+/* Import without { } because responseHandler is exported through export.default */
+import responseHandlerWithExportDefault from './response-handler.js';
+import { responseHandlerExportedDirectly } from './responseHandlerExportedDirectly.js';
 
 const app = express();
 
-app.get('/', responseHandler);
+app.get('/module-exports', responseHandlerWithExportDefault);
+app.get('/export-const-directly', responseHandlerExportedDirectly);
 
 app.listen(3000);
--- Studies/32-modern-js/responseHandlerExportedDirectly.js
+++ Studies/32-modern-js/responseHandlerExportedDirectly.js
@@ -1,11 +1,10 @@
 import fs from 'fs';
 // const fs = require('fs');
 
-const responseHandlerWithExportDefault = (req, res, next) => {
+export const responseHandlerExportedDirectly = (req, res, next) => {
     fs.readFile('my-page.html', 'utf8', (err, data) => {
       res.send(data);
     });
   }
 
-export default responseHandlerWithExportDefault;
 // module.exports = responseHandler;
