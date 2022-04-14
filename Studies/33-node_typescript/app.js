adf0c5426dd072998f0cbd4774bf6649e9abd2f6
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2022-04-14 12:33:24 -0300

    Adds types to function parameters

--- Studies/33-node_typescript/app.js
+++ Studies/33-node_typescript/app.js
@@ -2,4 +2,4 @@ function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(1, 6));
-console.log(add('1', '6'));
+// console.log(add('1', '6')); // will trigger 'Argument of type string is not assignable to type number
--- Studies/33-node_typescript/app.ts
+++ Studies/33-node_typescript/app.ts
@@ -1,7 +1,7 @@
-function add(num1, num2) {
+function add(num1: number, num2: number) {
     return num1 + num2;
 }
 
 console.log(add(1, 6));
 
-console.log(add('1', '6'));
+// console.log(add('1', '6')); // will trigger 'Argument of type string is not assignable to type number
