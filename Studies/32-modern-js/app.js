0214ba97f35d76d8d046c7edd281e4569359b0b9
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2022-04-14 12:12:02 -0300

    Using global variables with ES Modules

--- Studies/32-modern-js/app.js
+++ Studies/32-modern-js/app.js
@@ -4,11 +4,12 @@ import express from 'express';
 
 /* Import without { } because responseHandler is exported through export.default */
 import responseHandlerWithExportDefault_WithPromises from './response-handler.js';
-import { responseHandlerExportedDirectly_WithCallback } from './responseHandlerExportedDirectly.js';
+import { responseHandlerExportedDirectly_WithCallback, responseUsingGlobalVariables } from './responseHandlerExportedDirectly.js';
 
 const app = express();
 
 app.get('/module-exports', responseHandlerWithExportDefault_WithPromises);
 app.get('/export-const-directly', responseHandlerExportedDirectly_WithCallback);
+app.get('/global-variables', responseUsingGlobalVariables);
 
 app.listen(3000);
--- Studies/32-modern-js/responseHandlerExportedDirectly.js
+++ Studies/32-modern-js/responseHandlerExportedDirectly.js
@@ -1,10 +1,21 @@
 import fs from 'fs';
 // const fs = require('fs');
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 
 export const responseHandlerExportedDirectly_WithCallback = (req, res, next) => {
-    fs.readFile('my-page.html', 'utf8', (err, data) => {
-      res.send(data);
-    });
-  }
+  fs.readFile('my-page.html', 'utf8', (err, data) => {
+    res.send(data);
+  });
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const responseUsingGlobalVariables = (req, res, next) => {
+  // res.sendFile('my-page.html') // triggers an error because path must be absolute
+  res.sendFile(path.join(__dirname, 'my-page.html')) // It only work with commonJs, not with ES Modules (unless you create the __filename and __dirname consts on the top)
+}
 
 // module.exports = responseHandler;
