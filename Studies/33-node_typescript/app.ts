c347384bfa451f0a5ee1bc9cd7af0e4a77520dde
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2022-04-14 16:11:37 -0300

    Use TypeScript express to create a simple route

--- Studies/33-node_typescript/app.ts
+++ Studies/33-node_typescript/app.ts
@@ -1,5 +1,9 @@
 import express from 'express'; // typically import on client side
 
+import todosRoutes from './routes/todos'
+
 const app = express();
+
+app.use(todosRoutes);
 
 app.listen(3000); 
