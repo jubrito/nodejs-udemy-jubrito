0452a6961eae8d38aa403f828f1da19a6bf05f39
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2022-04-14 16:44:17 -0300

    Use type casting on Request Body and Params

--- Studies/33-node_typescript/routes/todos.ts
+++ Studies/33-node_typescript/routes/todos.ts
@@ -6,31 +6,38 @@ const router = Router();
 
 let todos: Todo[] = [];
 
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 })
 
 router.post('/todo', (req, res, next) => {
+    const body = req.body as RequestBody;
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text,
     }
     todos.push(newTodo);
     return res.status(201).json({ message: 'Added todo', todo: newTodo, todos: todos })
 })
 
 router.put('/todo/:todoId', (req, res, next) => {
-    const todoId = req.params.todoId;
+    const body = req.body as RequestBody;
+    const params = req.params as RequestParams;
+    const todoId = params.todoId;
     const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
     if (todoIndex >= 0) {
-        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text }
+        todos[todoIndex] = { id: todos[todoIndex].id, text: body.text }
         return res.status(200).json({ message: 'Updated todo', todos: todos })
     }
     res.status(404).json({ message: 'Could not find todo for this id' })
 })
 
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId)
+    const params = req.params as RequestParams;
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId)
     res.status(200).json({ message: 'Deleted todo', todos: todos })
 })
 
