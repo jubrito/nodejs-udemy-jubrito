4bdfa4b03afcb7ff656c024e88afb7f97b56053a
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2022-04-14 16:23:13 -0300

    Add delete todo route

--- Studies/33-node_typescript/routes/todos.ts
+++ Studies/33-node_typescript/routes/todos.ts
@@ -4,7 +4,7 @@ import { Todo } from '../models/todo';
 
 const router = Router();
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos })
@@ -26,7 +26,11 @@ router.put('/todo/:todoId', (req, res, next) => {
         return res.status(200).json({ message: 'Updated todo', todos: todos })
     }
     res.status(404).json({ message: 'Could not find todo for this id' })
+})
 
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId)
+    res.status(200).json({ message: 'Deleted todo', todos: todos })
 })
 
 export default router;
