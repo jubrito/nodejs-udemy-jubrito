02f5a61740c247a1de4616bc9c273add8d39e273
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2022-04-14 12:52:04 -0300

    Configure TypeScript and Add tsconfig.json file with strict = true

--- Studies/33-node_typescript/app.ts
+++ Studies/33-node_typescript/app.ts
@@ -1,6 +1,6 @@
 const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
-const buttonElement = document.querySelector('button') as HTMLButtonElement;
+const buttonElement = document.querySelector('button')!;
 
 function add(num1: number, num2: number) {
     return num1 + num2;
