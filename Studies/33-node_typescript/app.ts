3aecaac32dfb17ad0cb0d1b2908d34b683cd4c06
Juliana Witzke de Brito <juliana.witzke@sigfig.com>
2022-04-14 12:56:32 -0300

    Works with Union Types

--- Studies/33-node_typescript/app.ts
+++ Studies/33-node_typescript/app.ts
@@ -6,11 +6,26 @@ function add(num1: number, num2: number) {
     return num1 + num2;
 }
 
+function addWithUnion(num1: number | string, num2: number | string) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    } else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + '' + num2;
+    }
+    return +num1 + +num2;
+}
+
 buttonElement.addEventListener('click', () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
     console.log(result);
+    var resultWithUnion = addWithUnion('1', '2');
+    console.log(resultWithUnion);
+    resultWithUnion = addWithUnion(1, 2);
+    console.log(resultWithUnion);
+    resultWithUnion = addWithUnion(1, '2');
+    console.log(resultWithUnion);
 })
 
 console.log(add(1, 6));
