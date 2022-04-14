fec7f262cda3a6ce0aea8f441a8dc10b6d68d818
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2022-04-14 16:21:36 -0300

    Add a put route to change a todo

--- Studies/33-node_typescript/routes/todos.ts
+++ Studies/33-node_typescript/routes/todos.ts
@@ -15,6 +15,18 @@ router.post('/todo', (req, res, next) => {
         id: new Date().toISOString(),
         text: req.body.text
     }
+    return res.status(201).json({ message: 'Added todo', todo: newTodo, todos: todos })
+})
+
+router.put('/todo/:todoId', (req, res, next) => {
+    const todoId = req.params.todoId;
+    const todoIndex = todos.findIndex(todoItem => todoItem.id === todoId);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text }
+        return res.status(200).json({ message: 'Updated todo', todos: todos })
+    }
+    res.status(404).json({ message: 'Could not find todo for this id' })
+
 })
 
 export default router;
