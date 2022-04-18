52e62eaa64cde60c0b09ac49a4cbb45bcd55def2
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2022-04-18 16:07:42 -0300

    Rebuilds REST API with Oak

--- Studies/34-deno/deno/app.ts
+++ Studies/34-deno/deno/app.ts
@@ -3,6 +3,15 @@ import todosRoutes from './routes/todos.ts'; // Deno requires the file extention
 
 const app = new Application();
 
+app.use(async (ctx, next) => {
+    console.log('When this middleware is done, Oak will automatically send back a response.');
+    console.log('With next(), we might send back a response too early, before the route has been able to process the async requests.')
+    console.log('Therefore, whenever you have any middlewares that do async stuff, you should make all your middlewares async and always await next.');
+    console.log("Adding async await to this middleware tells Oak that we don't just want to start the next middlewares in line, but that we also want to wait for them to finish before we send back that automatically generated response.");
+    console.log("Otherwise, the response bodies set by our async route middlewares, will not be taken into account.")
+    await next();
+})
+
 /* We need to call the routes method on Deno and allow methods to make sure Deno and Oak will properly handle incoming requests to our routes */
 app.use(todosRoutes.routes());
 app.use(todosRoutes.allowedMethods());
--- Studies/34-deno/deno/routes/todos.ts
+++ Studies/34-deno/deno/routes/todos.ts
@@ -27,7 +27,18 @@ router.post('/todo', async (ctx) => {
     todos.push(newTodo);
     ctx.response.body = { message: "Created todo", todo: newTodo };
 });
-router.put('/todo/:todoId', (ctx) => {});
-router.delete('/todo/:todoId', (ctx) => {});
+router.put('/todo/:todoId', async (ctx) => {
+    const data = await ctx.request.body().value;
+    const todoId = ctx.params.todoId;
+    const todoIndex = todos.findIndex(todo => todo.id === todoId);
+    todos[todoIndex] = { id: todos[todoIndex].id, text: data.text };
+    ctx.response.body = { message: "Updated todo", todos: todos };
+});
+router.delete('/todo/:todoId', (ctx) => {
+    const todoId = ctx.params.todoId;
+    const todoIndex = todos.findIndex(todo => todo.id === todoId);
+    todos.filter(todo => todo.id !== todoId);
+    ctx.response.body = { message: 'Deleted todo', todos: todos };
+});
 
 export default router;
