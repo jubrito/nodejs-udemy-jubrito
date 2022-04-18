2c5a50675ae5989e3e638f9564ea0c05e7bb4b31
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2022-04-18 11:24:16 -0300

    Uses Runtime API to write files

--- Studies/34-deno/app.ts
+++ Studies/34-deno/app.ts
@@ -1,5 +1,14 @@
 let message: string;
 
-message = 'Hello World';
+message = "This is a test and should be stored in a file! If we try to write a file with Deno, writeFile wants two main arguments here, it wants a path to the file, including the file name and the data that should be written to the file. And the data should be a Uint8Array, which is a strange data type, but which is actually a core data type built into JavaScript, which in the end is an array full of bytes. Now, we will be able to convert our text to bytes. So it's no problem that we have no bytes right now, but that we only have a string";
+
+const fileName = 'message.txt';
+const encoder = new TextEncoder();
+const data_Uint8Array_ConvertingTextToBites = encoder.encode(message);
+Deno
+    .writeFile(fileName, data_Uint8Array_ConvertingTextToBites)
+    .then(() => {
+        console.log("Deno embraces modern JavaScript features like Promises. Therefore writeFile does not take a call back to let us know once it's done. Instead here, we now can call then or a catch because writeFile returns a promise.")
+    });
 
 console.log(message);
