3a45187cc088df7569fb6518875dc1c4194774a8
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2022-04-18 11:39:25 -0300

    Write files with Deno vs Node

---
+++ Studies/34-deno/app.js
@@ -0,0 +1,20 @@
+
+// const fs = require('fs');
+const fs = require('fs').promises;
+
+const fileName = 'node-message.txt';
+const message = "This is a test with Node and should be stored in a file!";
+
+// fs.writeFile(fileName, message, () => {
+//     console.log("One crucial difference between Deno and Node is that in Node, write file does now not return a promise, but that instead we pass in this callback here to have some code that executes once this completes or that tells us about a potential error that could have occurred.")
+//     console.log("If we added 'const fs = require('fs').promises' we would have the promise based version of the write file")
+// })
+
+fs
+    .writeFile(fileName, message)
+    .then(() => {
+        console.log('Wrote file with Node using Promises');
+        console.log("We now don't need to specify any permissions we wanna assign to that execution process (like we did with Deno). Instead by default as I mentioned before, node scripts, scripts executed with node by default, have full read and write access.")
+    })
+
+// Run this with node app.js
--- Studies/34-deno/app.ts
+++ Studies/34-deno/app.ts
@@ -1,8 +1,8 @@
 let message: string;
 
-message = "This is a test and should be stored in a file! \r\nIf we try to write a file with Deno, writeFile wants two main arguments here, it wants a path to the file, including the file name and the data that should be written to the file. And the data should be a Uint8Array, which is a strange data type, but which is actually a core data type built into JavaScript, which in the end is an array full of bytes. Now, we will be able to convert our text to bytes. So it's no problem that we have no bytes right now, but that we only have a string";
+message = "This is a test with Deno and should be stored in a file! \r\nIf we try to write a file with Deno, writeFile wants two main arguments here, it wants a path to the file, including the file name and the data that should be written to the file. And the data should be a Uint8Array, which is a strange data type, but which is actually a core data type built into JavaScript, which in the end is an array full of bytes. Now, we will be able to convert our text to bytes. So it's no problem that we have no bytes right now, but that we only have a string";
 
-const fileName = 'message.txt';
+const fileName = 'deno-message.txt';
 const encoder = new TextEncoder();
 const data_Uint8Array_ConvertingTextToBites = encoder.encode(message);
 Deno
@@ -11,4 +11,4 @@ Deno
         console.log("Deno embraces modern JavaScript features like Promises. Therefore writeFile does not take a call back to let us know once it's done. Instead here, we now can call then or a catch because writeFile returns a promise.")
     });
 
-console.log(message);
+// Run this with deno run app.ts
