e8dcdf7218f8edd2ddbd17cba30c6d454e705a0f
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2022-04-20 15:08:57 -0300

    Use Deno Mongo DB Crud for editing (PUT)

--- Studies/34-35-deno/deno/routes/todos.ts
+++ Studies/34-35-deno/deno/routes/todos.ts
@@ -41,16 +41,18 @@ router.post('/todo', async (ctx) => {
     const newTodo: Todo = { 
         text: data.text,
     }
-    const idOfNewTodoGeneratedByMongoDb = await getDb().collection('todos').insertOne(newTodo);
-    newTodo.id = idOfNewTodoGeneratedByMongoDb.$oid; 
+    const todoIdGeneratedByMongoDb = await getDb().collection('todos').insertOne(newTodo);
+    newTodo.id = todoIdGeneratedByMongoDb.$oid; 
     ctx.response.body = { message: "Created todo", todo: newTodo };
 });
 router.put('/todo/:todoId', async (ctx) => {
     const data = await ctx.request.body().value;
-    const todoId = ctx.params.todoId;
-    const todoIndex = todos.findIndex(todo => todo.id === todoId);
-    todos[todoIndex] = { id: todos[todoIndex].id, text: data.text };
-    ctx.response.body = { message: "Updated todo", todos: todos };
+    const todoId = ctx.params.todoId!;
+
+    const filter = { _id: new ObjectId(todoId) };
+    const updateInstructions = { $set: { text: data.text }}
+    const todo = await getDb().collection('todos').updateOne(filter, updateInstructions);
+    ctx.response.body = { message: "Updated todo", todo: todo };
 });
 router.delete('/todo/:todoId', (ctx) => {
     const todoId = ctx.params.todoId;
